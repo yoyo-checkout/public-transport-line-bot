5d58c8275bc70bee6093163671bd307e33364905
zongyo <stem597sag445@gmail.com>
2020-11-08 15:03:35 +0800

    init line bot setting

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const linebot = require('linebot');
 const express = require('express');
 const dotenv = require('dotenv').config();
 
-console.log(process.env.CHANNEL_ID);
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
 
-// const bot = linebot({
-//   channelId: channel Id,
-//   channelSecret: channel Secret,
-//   channelAccessToken: channel Access Token
-// });
+// 當有人傳送訊息給 Bot 時
+bot.on('message', event => {
+  // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
+  event.reply(`你說了 ${event.message.text}`);
+});
+
+// Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
+bot.listen('/', process.env.PORT || 5000, () => {
+  console.log('全國首家LINE線上機器人上線啦！！');
+});
