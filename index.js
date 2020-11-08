d3580345b57542ea4a0b6f02f339669d6cac4568
zongyo <stem597sag445@gmail.com>
2020-11-08 21:13:55 +0800

    reply for text message

--- index.js
+++ index.js
@@ -12,6 +12,109 @@ bot.on('message', event => {
   event.reply(`你說了 ${event.message.text}.`);
 });
 
+
+
+bot.on('message', function (event) {
+  switch (event.message.type) {
+    case 'text':
+      switch (event.message.text) {
+        case '台鐵即時站點資訊':
+          event.reply('台鐵即時站點資訊')
+          break;
+        case 'Me':
+          event.source.profile().then(function (profile) {
+            return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
+          });
+          break;
+        case 'Member':
+          event.source.member().then(function (member) {
+            return event.reply(JSON.stringify(member));
+          });
+          break;
+        case 'Picture':
+          event.reply({
+            type: 'image',
+            originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
+            previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
+          });
+          break;
+        case 'Location':
+          event.reply({
+            type: 'location',
+            title: 'LINE Plus Corporation',
+            address: '1 Empire tower, Sathorn, Bangkok 10120, Thailand',
+            latitude: 13.7202068,
+            longitude: 100.5298698
+          });
+          break;
+        case 'Push':
+          bot.push('U17448c796a01b715d293c34810985a4c', ['Hey!', 'สวัสดี ' + String.fromCharCode(0xD83D, 0xDE01)]);
+          break;
+        case 'Push2':
+          bot.push('Cba71ba25dafbd6a1472c655fe22979e2', 'Push to group');
+          break;
+        case 'Multicast':
+          bot.push(['U17448c796a01b715d293c34810985a4c', 'Cba71ba25dafbd6a1472c655fe22979e2'], 'Multicast!');
+          break;
+        case 'Broadcast':
+          bot.broadcast('Broadcast!');
+          break;
+        case 'Confirm':
+          event.reply({
+            type: 'template',
+            altText: 'this is a confirm template',
+            template: {
+              type: 'confirm',
+              text: 'Are you sure?',
+              actions: [{
+                type: 'message',
+                label: 'Yes',
+                text: 'yes'
+              }, {
+                type: 'message',
+                label: 'No',
+                text: 'no'
+              }]
+            }
+          });
+          break;
+        case 'Multiple':
+          return event.reply(['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']);
+        case 'Total followers':
+          bot.getTotalFollowers().then((result) => {
+            event.reply('Total followers: ' + result.followers);
+          });
+          break;
+        case 'Quota':
+          bot.getQuota().then((result) => {
+            event.reply('Quota: ' + result.value);
+          });
+          break;
+        case 'Total reply':
+          bot.getTotalReplyMessages().then((result) => {
+            event.reply('Total reply messages: ' + result.success);
+          });
+          break;
+        case 'Version':
+          event.reply('linebot@' + require('../package.json').version);
+          break;
+        default:
+          event.reply(event.message.text).then(function (data) {
+            console.log('Success', data);
+          }).catch(function (error) {
+            console.log('Error', error);
+          });
+          break;
+      }
+      break;
+    default:
+      event.reply('很抱歉，我們無法理解您的疑問，請換個方式再問一次。');
+      break;
+  }
+});
+
+
+
 // Bot 所監聽的 webhook 路徑與 port
 // heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
 bot.listen('/', process.env.PORT || 5000, function () {
