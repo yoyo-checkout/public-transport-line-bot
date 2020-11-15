b341bb30b7b60f535aa8a6a2e1dbd8f9271f227d
zongyo <stem597sag445@gmail.com>
2020-11-15 20:58:18 +0800

    complete flex message

--- index.js
+++ index.js
@@ -40,29 +40,329 @@ bot.on('message', async (event) => {
                     body: {
                       type: 'box',
                       layout: 'vertical',
+                      paddingAll: 'md',
+                      paddingBottom: 'none',
                       contents: [
                         {
                           type: 'text',
-                          text: '北區',
+                          text: '北北基',
                           weight: 'bold',
                           size: 'xl',
                           align: 'center',
                         },
-                      ],
-                    },
-                    footer: {
-                      type: 'box',
-                      layout: 'vertical',
-                      spacing: 'sm',
-                      contents: [
                         {
                           type: 'button',
                           style: 'link',
                           height: 'sm',
                           action: {
-                            type: 'message',
+                            type: 'postback',
                             label: '台北市',
-                            text: '查詢台北市內車站',
+                            data: 'type=tra&action=getDistric&district=TPE',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '新北市',
+                            data: 'type=tra&action=getDistric&district=TPH',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '基隆市',
+                            data: 'type=tra&action=getDistric&district=KLU',
+                          },
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    type: 'bubble',
+                    hero: {
+                      type: 'image',
+                      url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+                      size: 'full',
+                      aspectRatio: '20:13',
+                      aspectMode: 'cover',
+                    },
+                    body: {
+                      type: 'box',
+                      layout: 'vertical',
+                      paddingAll: 'md',
+                      paddingBottom: 'none',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '桃竹苗',
+                          weight: 'bold',
+                          size: 'xl',
+                          align: 'center',
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '桃園市',
+                            data: 'type=tra&action=getDistric&district=TYC',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '新竹市',
+                            data: 'type=tra&action=getDistric&district=HSC',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '新竹縣',
+                            data: 'type=tra&action=getDistric&district=HSH',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '苗栗縣',
+                            data: 'type=tra&action=getDistric&district=MAL',
+                          },
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    type: 'bubble',
+                    hero: {
+                      type: 'image',
+                      url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+                      size: 'full',
+                      aspectRatio: '20:13',
+                      aspectMode: 'cover',
+                    },
+                    body: {
+                      type: 'box',
+                      layout: 'vertical',
+                      paddingAll: 'md',
+                      paddingBottom: 'none',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '中彰投',
+                          weight: 'bold',
+                          size: 'xl',
+                          align: 'center',
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '台中市',
+                            data: 'type=tra&action=getDistric&district=TXG',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '彰化縣',
+                            data: 'type=tra&action=getDistric&district=CWH',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '南投縣',
+                            data: 'type=tra&action=getDistric&district=NTO',
+                          },
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    type: 'bubble',
+                    hero: {
+                      type: 'image',
+                      url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+                      size: 'full',
+                      aspectRatio: '20:13',
+                      aspectMode: 'cover',
+                    },
+                    body: {
+                      type: 'box',
+                      layout: 'vertical',
+                      paddingAll: 'md',
+                      paddingBottom: 'none',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '雲嘉南',
+                          weight: 'bold',
+                          size: 'xl',
+                          align: 'center',
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '雲林縣',
+                            data: 'type=tra&action=getDistric&district=YLH',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '嘉義市',
+                            data: 'type=tra&action=getDistric&district=CYI',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '嘉義縣',
+                            data: 'type=tra&action=getDistric&district=CHY',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '台南市',
+                            data: 'type=tra&action=getDistric&district=TNN',
+                          },
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    type: 'bubble',
+                    hero: {
+                      type: 'image',
+                      url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+                      size: 'full',
+                      aspectRatio: '20:13',
+                      aspectMode: 'cover',
+                    },
+                    body: {
+                      type: 'box',
+                      layout: 'vertical',
+                      paddingAll: 'md',
+                      paddingBottom: 'none',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '高屏',
+                          weight: 'bold',
+                          size: 'xl',
+                          align: 'center',
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '高雄市',
+                            data: 'type=tra&action=getDistric&district=KHH',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '屏東縣',
+                            data: 'type=tra&action=getDistric&district=IUH',
+                          },
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    type: 'bubble',
+                    hero: {
+                      type: 'image',
+                      url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+                      size: 'full',
+                      aspectRatio: '20:13',
+                      aspectMode: 'cover',
+                    },
+                    body: {
+                      type: 'box',
+                      layout: 'vertical',
+                      paddingAll: 'md',
+                      paddingBottom: 'none',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: '宜花東',
+                          weight: 'bold',
+                          size: 'xl',
+                          align: 'center',
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '宜蘭縣',
+                            data: 'type=tra&action=getDistric&district=ILN',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '花蓮縣',
+                            data: 'type=tra&action=getDistric&district=HWA',
+                          },
+                        },
+                        {
+                          type: 'button',
+                          style: 'link',
+                          height: 'sm',
+                          action: {
+                            type: 'postback',
+                            label: '台東縣',
+                            data: 'type=tra&action=getDistric&district=TTT',
                           },
                         },
                       ],
@@ -71,156 +371,6 @@ bot.on('message', async (event) => {
                 ],
               },
             });
-
-            // event.reply({
-            //   type: 'template',
-            //   altText: '很抱歉，我們不支援此裝置',
-            //   template: {
-            //     type: 'carousel',
-            //     columns: [ // max 10
-            //       {
-            //         thumbnailImageUrl: 'https://fakeimg.pl/300/',
-            //         text: '北北基',
-            //         actions: [ // max 3
-            //           {
-            //             type: 'postback',
-            //             label: '台北市',
-            //             data: 'type=tra&action=getDistric&district=TPE',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '新北市',
-            //             data: 'type=tra&action=getDistric&district=TPH',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '基隆市',
-            //             data: 'type=tra&action=getDistric&district=KLU',
-            //           },
-            //           // {
-            //           //   type: 'message',
-            //           //   label: 'Buy',
-            //           //   text: 'message',
-            //           // },
-            //           // {
-            //           //   type: 'datetimepicker',
-            //           //   label: 'Select date',
-            //           //   data: 'storeId=12345',
-            //           //   mode: 'time',
-            //           //   initial: '00:00',
-            //           //   max: '23:59',
-            //           //   min: '00:00',
-            //           // },
-            //         ],
-            //       },
-            //       {
-            //         thumbnailImageUrl: 'https://fakeimg.pl/300/',
-            //         text: '桃竹',
-            //         actions: [
-            //           {
-            //             type: 'postback',
-            //             label: '桃園市',
-            //             data: 'type=tra&action=getDistric&district=TYC',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '新竹縣',
-            //             data: 'type=tra&action=getDistric&district=HSH',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '新竹市',
-            //             data: 'type=tra&action=getDistric&district=HSC',
-            //           },
-            //         ],
-            //       },
-            //       {
-            //         thumbnailImageUrl: 'https://fakeimg.pl/300/',
-            //         text: '苗中彰',
-            //         actions: [
-            //           {
-            //             type: 'postback',
-            //             label: '苗栗縣',
-            //             data: 'type=tra&action=getDistric&district=MAL',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '台中市',
-            //             data: 'type=tra&action=getDistric&district=TXG',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '彰化縣',
-            //             data: 'type=tra&action=getDistric&district=CWH',
-            //           },
-            //         ],
-            //       },
-            //       {
-            //         thumbnailImageUrl: 'https://fakeimg.pl/300/',
-            //         text: '雲嘉',
-            //         actions: [
-            //           {
-            //             type: 'postback',
-            //             label: '雲林縣',
-            //             data: 'type=tra&action=getDistric&district=YLH',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '嘉義縣',
-            //             data: 'type=tra&action=getDistric&district=CHY',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '嘉義市',
-            //             data: 'type=tra&action=getDistric&district=CYI',
-            //           },
-            //         ],
-            //       },
-            //       {
-            //         thumbnailImageUrl: 'https://fakeimg.pl/300/',
-            //         text: '南高屏',
-            //         actions: [
-            //           {
-            //             type: 'postback',
-            //             label: '台南市',
-            //             data: 'type=tra&action=getDistric&district=TNN',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '高雄市',
-            //             data: 'type=tra&action=getDistric&district=KHH',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '屏東縣',
-            //             data: 'type=tra&action=getDistric&district=IUH',
-            //           },
-            //         ],
-            //       },
-            //       {
-            //         thumbnailImageUrl: 'https://fakeimg.pl/300/',
-            //         text: '宜花東',
-            //         actions: [
-            //           {
-            //             type: 'postback',
-            //             label: '宜蘭縣',
-            //             data: 'type=tra&action=getDistric&district=ILN',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '花蓮縣',
-            //             data: 'type=tra&action=getDistric&district=HWA',
-            //           },
-            //           {
-            //             type: 'postback',
-            //             label: '台東縣',
-            //             data: 'type=tra&action=getDistric&district=TTT',
-            //           },
-            //         ],
-            //       },
-            //     ],
-            //   },
-            // });
           } catch (error) {
             console.log(error);
             event.reply('系統繁忙中，請稍後再試');
